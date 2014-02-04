744d1ed5d459a0fae5d4208c60492d18802efa4d
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-02-04 12:56:02 +0900

    フォーマット適応

--- Assets/Demo/Script/MoveTank.cs
+++ Assets/Demo/Script/MoveTank.cs
@@ -1,27 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveTank : MonoBehaviour {
-
+public class MoveTank : MonoBehaviour
+{
 	private Vector2 input;
 	public float speed = 1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		input.x = Input.GetAxis("Horizontal");
-		input.y = Input.GetAxis("Vertical");
-	}
-
-	void FixedUpdate()
+	void Update ()
 	{
-		//rigidbody.MoveRotation(Quaternion.FromToRotation(Vector3.forward, new Vector3(input.x, input.y)));
-		transform.Rotate(Vector3.up, input.x * speed);
-		rigidbody.velocity =  transform.forward * input.y * speed;
+		input.x = Input.GetAxis ("Horizontal");
+		input.y = Input.GetAxis ("Vertical");
+	}
+
+	void FixedUpdate ()
+	{
+		transform.Rotate (Vector3.up, input.x * speed);
+		rigidbody.velocity = transform.forward * input.y * speed;
 	}
 }
