b17f6e24501afcc111875c14b2782c45199c4377
山村 達彦 <ruitomo.ex@gmail.com>
2014-02-07 00:25:07 +0900

    車・戦車の移動を追加

---
+++ Assets/Demo/Script/MoveAgent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAgent : MonoBehaviour {
+
+	private Transform target;
+	private NavMeshAgent agent;
+
+	void Start () {
+		var t = GameObject.FindWithTag("Player");
+		target = t.transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(target.position);
+	}
+}
