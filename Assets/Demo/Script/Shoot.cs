a0c89c9cdf4022700d2dd23018caf3a2cdc58eb1
山村 達彦 <ruitomo.ex@gmail.com>
2014-02-07 00:21:12 +0900

    弾を発射して対象を非アクティブにする機能を追加

---
+++ Assets/Demo/Script/Shoot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	[SerializeField]
+	Light light;
+
+	[SerializeField]
+	public float intervalFire, intervalLight;
+
+	private float currentFire, currentLight;
+
+	public LayerMask mask;
+
+	// Update is called once per frame
+	void Update () {
+		float time = Time.time;
+		if( Input.GetButton("Fire1") &&  time > currentFire)
+		{
+			currentFire = time + intervalFire;
+			currentLight = time + intervalLight;
+			light.enabled = true;
+			light.intensity = 8;
+
+			RaycastHit hit;
+			if( Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, 50, mask))
+			{
+				if( hit.collider.CompareTag("Enemy") )
+					hit.collider.attachedRigidbody.gameObject.SetActive(false);
+			}
+		}
+
+		if( time > currentLight )
+		{
+			light.enabled = false;
+		}else{
+			light.intensity /= 1.5f;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawRay(transform.position + Vector3.up , transform.forward * 50);
+	}
+
+}
